64e84c3578d3a50ba55eb82765a6de2ebc3c78c1
Ronnie <167436841+Iwis19@users.noreply.github.com>
2026-04-19 23:54:31 -0400

    Create 2078-TwoFurthestHousesWithDifferentColors (two ptr).py

---
+++ 2078-TwoFurthestHousesWithDifferentColors/2078-TwoFurthestHousesWithDifferentColors (two ptr).py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxDistance(self, colors: List[int]) -> int:
+
+        """
+        double two pointerrrr
+
+        1. had a bit of small trouble working with indices like len(colors) - l - 1, other than that im optimal yay
+
+        0 ms runtime beats 100
+        """
+        
+        l, r = 0, len(colors)-1
+        res = 0
+
+        while r > 0:
+
+            if colors[0] == colors[r]:
+                r -= 1
+                continue
+            
+            res = max(res, r)
+            break
+
+        while l < len(colors):
+
+            if colors[l] == colors[len(colors)-1]:
+                l += 1
+                continue
+            
+            res = max(res, len(colors)-l-1)
+            break
+
+        return res
