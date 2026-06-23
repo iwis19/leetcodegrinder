a5c0ec69e61126aa1624577db9e622c2617c380e
Ronnie <167436841+Iwis19@users.noreply.github.com>
2026-06-23 17:57:45 -0400

    Update 90-SubsetsII (if statement).py

--- 90-SubsetsII/90-SubsetsII (if statement).py
+++ 90-SubsetsII/90-SubsetsII (if statement).py
@@ -2,7 +2,12 @@ class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
 
         """
-        not using sets, using a check, will MAKE SURE TO READ THE REASONING TOMORROW.
+        not using sets, using a check.
+
+        uses i > start and checks if current num is the same as prev, if so, skip. this effectively prevents reproducing the same outcomes from a sibling choice
+        e.g. nums = [1,2,2]
+        we could already have a [1,2] -> with 2 at i = 1, but we coudl aslo have a [1,2] -> with 2 at i = 2. by having that if statement, it blocks a sibling choice.
+        [1,2,2] will still be not impacted and produced as normally as it passes the check since it allowws me to regualrly move onto the next start.
 
         need to get better at these checks, but now i know do not use sets as theyre inefficient even tho theyre o(1) lookup, recursion brings too much burden
 
